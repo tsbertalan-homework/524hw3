7097c930bf1d9887dba19e81fdb33e461ec9cbbe
Tom S. Bertalan <tom@tombertalan.com>
2012-10-23 20:06:37 -0400

    More docstrings.

--- testNewton.py
+++ testNewton.py
@@ -7,6 +7,7 @@ import numpy as np
 
 class TestNewton(unittest.TestCase):
     def testLinear(self):
+        '''Tests newton.solve() with a linear function of one variable.'''
         f = lambda x: 3.0 * x + 6.0
         solver = newton.Newton(f, tol=1.e-15, maxiter=2)
         x = solver.solve(2.0)
