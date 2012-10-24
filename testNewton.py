e997b626d3a4015323b851a3a801d29dcb93b147
Tom S. Bertalan <tom@tombertalan.com>
2012-10-23 20:54:03 -0400

    1-dimensional test of newton.step()
    Also spruced up a docstring.

--- testNewton.py
+++ testNewton.py
@@ -14,11 +14,20 @@ class TestNewton(unittest.TestCase):
         self.assertEqual(x, -2.0)
 
     def testStep1Var(self):
-        '''Tests newton.step() with a single-variable linear function.'''
-        pass
+        '''Tests newton.step() with a single-variable linear function.
+        x_{k+1} = x - Df(x)^{-1}*f(x)'''
+        slope = 9.0
+        f = lambda x: slope * x + 4.0
+        solver = newton.Newton(f)
+        x0 = 4.0
+        stepresult = solver.step(x0)
+        correct = x0 - f(x0) / slope
+        np.testing.assert_array_almost_equal(stepresult, np.matrix([[correct]]))
 
     def testStepNVar(self):
-        '''Tests newton.step() with linear functions of multiple variables.'''
+        '''Tests newton.step() with linear functions of multiple variables.
+        x_{k+1} = x - Df(x)^{-1}*f(x)'''
+        slope_matrix = np.matrix([[5.0 12.8; 15.]])
         pass
 
     def testFunctionKwarg(self):
