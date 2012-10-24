109acf120711223801d50bdf6bcc0b8676c5658f
Tom S. Bertalan <tom@tombertalan.com>
2012-10-23 23:43:21 -0400

    Apparently, for some numerical reason, the linear case needs maxiter=3 in order to not throw ValueError

--- testNewton.py
+++ testNewton.py
@@ -9,7 +9,7 @@ class TestNewton(unittest.TestCase):
     def testLinear(self):
         '''Tests newton.solve() with a linear function of one variable.'''
         f = lambda x: 3.0 * x + 6.0
-        solver = newton.Newton(f, tol=1.e-15, maxiter=2)
+        solver = newton.Newton(f, tol=1.e-15, maxiter=3)
         x = solver.solve(2.0)
         self.assertEqual(x, -2.0)
 
