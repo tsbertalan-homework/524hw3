70ecf3e5f58c22268f80b5bb1861e1ad0f691e73
Tom S. Bertalan <tom@tombertalan.com>
2012-10-23 21:18:08 -0400

    Testing with 20 variables
    random coefficients (np.random.rand)

--- testNewton.py
+++ testNewton.py
@@ -25,8 +25,8 @@ class TestNewton(unittest.TestCase):
         correct = x0 - f(x0) / slope
         np.testing.assert_array_almost_equal(stepresult, np.matrix([[correct]]))
 
-    def testStepNVar(self):
-        '''Tests newton.step() with linear functions of multiple variables.
+    def testStep2Var(self):
+        '''Tests newton.step() with linear functions of two variables.
         x_{k+1} = x - Df(x)^{-1}*f(x)'''
         slope_matrix = np.matrix([[5.0, 12.8], [15.90, 3.14159]])
         intercept_matrix = np.matrix([[12], [16]])
@@ -34,7 +34,21 @@ class TestNewton(unittest.TestCase):
         solver = newton.Newton(f)
         x0 = np.matrix([[3.0], [5.6]])
         stepresult = solver.step(x0)
-        correct = x0 - np.linalg.solve(slope_matrix, f(x0))
+#        correct = x0 - np.linalg.solve(slope_matrix, f(x0))  # This also works.
+        correct = x0 - np.dot(np.linalg.inv(slope_matrix), f(x0))
+        np.testing.assert_array_almost_equal(stepresult, correct)
+
+    def testStepBigVar(self):
+        '''Tests newton.step() with linear functions of many variables.
+        x_{k+1} = x - Df(x)^{-1}*f(x)'''
+        N = 100
+        slope_matrix = np.random.rand(N, N)
+        intercept_matrix = np.random.rand(N, 1)
+        f = lambda x: np.dot(slope_matrix, x) + intercept_matrix
+        solver = newton.Newton(f)
+        x0 = np.random.rand(N, 1)
+        stepresult = solver.step(x0)
+        correct = x0 - np.dot(np.linalg.inv(slope_matrix), f(x0))
         np.testing.assert_array_almost_equal(stepresult, correct)
 
     def testFunctionKwarg(self):
